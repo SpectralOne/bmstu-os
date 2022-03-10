01b745022a1be64d3abd74858d3aec050622c68e
Artyom Bogachenco <artyombogachenco@gmail.com>
2022-03-10 10:32:51 +0300

    lab2 inode

--- sem6/lab2/src/main.c
+++ sem6/lab2/src/main.c
@@ -3,10 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <time.h>
+#define __USE_XOPEN
+#define __USE_XOPEN_EXTENDED
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
 
 #include "color.h"
 
@@ -16,10 +18,8 @@
 
 static char __color_s[1488];
 
-typedef int __handler_fn_t(const char *filename, 
-                           const struct stat *st,
-                           int depth, 
-                           int type);
+typedef int __handler_fn_t(const char *filename, const struct stat *st,
+                           int depth, int type);
 
 static char *random_color();
 static __handler_fn_t work_handler;
@@ -27,7 +27,7 @@ static int dopath(const char *filename, int depth, __handler_fn_t *);
 
 int main(const int argc, const char **argv) {
     srand(time(NULL));
-    
+
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <dir>\n", argv[0]);
         return EXIT_FAILURE;
@@ -36,9 +36,7 @@ int main(const int argc, const char **argv) {
     return dopath(argv[1], 0, work_handler);
 }
 
-static int dopath(const char *filename, 
-                  int depth, 
-                  __handler_fn_t *func) {
+static int dopath(const char *filename, int depth, __handler_fn_t *func) {
     struct stat statbuf;
 
     if (lstat(filename, &statbuf) == -1) {
@@ -64,9 +62,11 @@ static int dopath(const char *filename,
     }
 
     chdir(filename);
-
+    int dir = (strcmp(filename, ".") == 0 || strcmp(filename, "..") == 0);
     struct dirent *dentry;
-    while ((dentry = readdir(dp)) != NULL) {
+    while (!dir && (dentry = readdir(dp)) != NULL) {
+        // Рязанова хочет убрать это из цикла
+        // можно до входа в цикл проверить имя файла
         if (strcmp(dentry->d_name, ".") == 0 ||
             strcmp(dentry->d_name, "..") == 0) {
             continue;
@@ -88,9 +88,7 @@ static int dopath(const char *filename,
     return status;
 }
 
-static int work_handler(const char *filename, 
-                        const struct stat *st, 
-                        int depth,
+static int work_handler(const char *filename, const struct stat *st, int depth,
                         int type) {
     for (int i = 0; i + 1 < depth; ++i) {
         printf("│   ");
@@ -100,9 +98,9 @@ static int work_handler(const char *filename,
 
     switch (type) {
         case __NOT_DIR:
-        #ifdef __RND_COLOR
+#ifdef __RND_COLOR
             s = random_color();
-        #else
+#else
             switch (st->st_mode & S_IFMT) {
                 case S_IFREG:
                     s = st->st_mode & S_IXUSR ? COLOR_EXE : COLOR_REG;
@@ -130,12 +128,12 @@ static int work_handler(const char *filename,
                             st->st_mode & S_IFMT);
                     return EXIT_FAILURE;
             }
-        #endif
-            printf("├── %s%s%s\n", s, filename, COLOR_RESET);
+#endif
+            printf("├── %s%s%s %d\n", s, filename, COLOR_RESET, st->st_ino);
             break;
         case __DIR:
-            printf("%s%s%s%s\n", depth ? "├── " : "", COLOR_DIR, filename,
-                   COLOR_RESET);
+            printf("%s%s%s%s %d\n", depth ? "├── " : "", COLOR_DIR, filename,
+                   COLOR_RESET, st->st_ino);
             break;
         case __DENIED:
             fprintf(stderr, "Can't read directory %s: %s\n", filename,
